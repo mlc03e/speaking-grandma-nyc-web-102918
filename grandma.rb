4b003104d5e66e9ab5af534e33846810f9e7268f
Meghan Campbell <mlc03e@gmail.com>
2018-10-11 17:05:33 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,10 @@
 def speak_to_grandma(phrase)
-    if phrase.upcase
-      return "NO, NOT SINCE 1938!"
-    elsif phrase == "I LOVE YOU GRANDMA!"
+    if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
+    
+    elseif phrase.upcase
+      return "NO, NOT SINCE 1938!"
+    
     else
       return "HUH?! SPEAK UP, SONNY!"
  end
