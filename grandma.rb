8b0a8dd37c75f22d0ac02705fb4c5d735bcf52ff
Meghan Campbell <mlc03e@gmail.com>
2018-10-11 16:52:47 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
     if phrase.upcase
       return "NO, NOT SINCE 1938!"
-    elseif phrase == "I LOVE YOU GRANDMA!"
+    elsif phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-      else
+    else
       return "HUH?! SPEAK UP, SONYY!"
  end
     # Write a speak_to_grandma method.
